e996501c20d3e54f7e51a5bd60c0f37a1976589f
Rachel Li <825255583@qq.com>
2017-03-05 14:34:23 +0800

    deploy

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './routes';
-import './main.css'
-import './post.css'
-import 'highlight.js/styles/magula.css'
-
-render(<Routers />,document.getElementById('root'));
